a26b6a3e63290006277b4bd21b080ad98b08916b
evepoi <55119879+evepoi@users.noreply.github.com>
2025-11-24 10:07:01 +0900

    2025.11.24 / 커밋 배포 6차

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-import { fileURLToPath, URL } from 'node:url' // 파일 경로 유틸리티
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite' // Vite 설정 도우미
-import vue from '@vitejs/plugin-vue' // Vue 3 플러그인
-import vueDevTools from 'vite-plugin-vue-devtools' // Vue DevTools 플러그인
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // GitHub Pages 배포를 위한 기본 URL 설정
-  // 'DBLibrery.github.io/Adminpage/' 형식의 URL에 맞춰 저장소 이름을 base 경로로 지정
+  // '/Adminpage/' 형태로 반드시 저장소 이름과 일치해야 합니다.
   base: '/Adminpage/', 
   
   // 프로젝트에서 사용할 Vite 플러그인 목록
   plugins: [
-    vue(),        // Vue 컴포넌트 처리
-    vueDevTools(),// Vue 개발자 도구 지원 (개발 환경에서만 활성화될 수 있음)
+    vue(),
+    vueDevTools(),
   ],
 
   // 경로 별칭 설정 (주로 @/src 경로를 더 짧게 사용하기 위함)
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)) // @를 src 폴더로 매핑
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
 
